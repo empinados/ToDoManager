707c3bdec1bca1faac83df611f76f92252b3b93c
llontores <ezaugenrikh@yandex.kz>
2026-02-12 00:50:08 +0500

    build fix

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ set -o errexit
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 
+python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
